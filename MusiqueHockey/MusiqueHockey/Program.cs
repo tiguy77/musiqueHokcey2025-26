c9ace09d18bdc22df6a6b73dc985907a88bb7c24
tiguy77 <80557286+tiguy77@users.noreply.github.com>
2026-09-25 14:16:52 -0400

    Garder le démarrage WinForms sur le thread STA

--- MusiqueHockey/MusiqueHockey/Program.cs
+++ MusiqueHockey/MusiqueHockey/Program.cs
@@ -6,7 +6,7 @@ namespace MusiqueHockey
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static async Task Main()
+        static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
@@ -21,7 +21,7 @@ namespace MusiqueHockey
             var database = new Services.DatabaseService(connectionString);
             try
             {
-                await database.InitializeAsync();
+                database.InitializeAsync().GetAwaiter().GetResult();
             }
             catch (Exception exception)
             {
